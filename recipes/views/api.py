eb9146469018e69d1b3109157a51ed0c27c4c764
Luis Henrique Bonini Santiago <luishbsantiago@gmail.com>
2025-03-09 18:47:42 -0300

    create api_list and api_detail

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
+from ..models import Recipe
+from ..serializers import RecipeSerializer
 
-#para permitir outros tipos de request @api_view(http_method_names=['get','post'])
+#Allow other types of request @api_view(http_method_names=['get','post'])
 @api_view()
 def recipe_api_list(request):
-  return Response("ok")
+  # Gets the first 10 published recipes using the get_published method from the Recipe model. Serializes the obtained recipes to a format suitable for JSON response. Returns the serialized response as a DRF Response object
+  recipes = Recipe.objects.get_published()[:10]
+  serializer = RecipeSerializer(instance=recipes, many=True)
+  return Response(serializer.data)
+
+@api_view()
+def recipe_api_detail(request, pk):
+  recipe = get_object_or_404(
+    Recipe.objects.get_published(),
+    pk=pk
+  )
+  serializer = RecipeSerializer(instance=recipe, many=False)
+  return Response(serializer.data)
